7c189ea1976a28a87cbfcbfb7f8fdd33702fedf5
ezra kipchirchir <ezrakipchirchir1034@gmail.com>
2022-06-23 13:04:46 +0300

    add form

---
+++ index.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoad',()=>{
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+  }
+  
+  function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+  }
+})
