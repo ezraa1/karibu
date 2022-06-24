a824f2a570b13bc0dc43224e35788297f062606f
ezra kipchirchir <ezrakipchirchir1034@gmail.com>
2022-06-24 15:46:08 +0300

    add js function to display content

--- index.js
+++ index.js
@@ -1,4 +1,26 @@
-// document.addEventListener('DOMContentLoad',()=>{
+
+
+
+const getPage = async () => {
+    fetch("https://api.m3o.com/v1/user/Create")
+    
+    
+      .then(response => response.json())
+       .then(data => console.log(data));
+}
+function displayMyContent(){
+    var myContent= document.getElementById("section1");
+    var defaultDisplay = myContent.style.display;
+    var servicesButton = document.getElementById ("servicesButton");
+    if (defaultDisplay == 'block') {
+        myContent.style.display = 'none';
+        // servicesButton.innerHTML
+    }
+    else{
+        myContent.style.display = 'block';
+        // servicesButton.innerHTML
+    }
+}
 function openForm() {
     document.getElementById("myForm").style.display = "block";
   }
@@ -6,4 +28,3 @@ function openForm() {
   function closeForm() {
     document.getElementById("myForm").style.display = "none";
   }
-// })
